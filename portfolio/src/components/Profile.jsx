cf06bfb66971becf7d048bbea2fd2c77f485ed4d
Ömer Bayram <emaryabo@gmail.com>
2024-10-09 16:12:24 +0300

    profile and project components optimized

--- portfolio/src/components/Profile.jsx
+++ portfolio/src/components/Profile.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
-import ProfileContext from "./ProfileContext";
+
 import { basicInfoSelector } from "../store/selectors/selectors";
+import BasicInfo from "./BasicInfo";
+import AboutMe from "./AboutMe";
 
 export default function Profile() {
   const info = useSelector(basicInfoSelector);
@@ -9,7 +11,17 @@ export default function Profile() {
       <div className="w-[960px] text-left font-bold text-[#CBF281] max-lg:text-center mb-3">
         <h1>{info.profileTitle}</h1>
       </div>
-      <ProfileContext />
+      <div className="max-w-[960px] min-h-[290.68px] flex gap-[30px]  bg-transparent max-md:flex-col max-md:h-auto">
+      <BasicInfo />
+      <div className="w-[300px] h-[290.68px] flex items-stretch justify-center max-lg:hidden">
+        <img
+          className="object-cover rounded-[10px] shadow-custom1"
+          src="./image.png"
+          alt="image"
+        />
+      </div>
+      <AboutMe />
+    </div>
     </div>
   );
 }
